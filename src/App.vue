00f2ebd6707413c6cefd6043ee95123cf47c7f8b
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-01-26 16:02:14 +0100

    Project creation - Added Title and Jumbotron

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Title/>
+    <Hero/>
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import Hero from './components/Hero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    Hero,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hero">
+    <img src="https://vuejs.org/images/logo.svg">
+    <div class="vue-info">
+        <h1>The Progressive JavaScript Framework </h1>
+        <a href="../assets/logo.png" target="blank">Vai al Sito</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<style scoped lang="scss">
+    * {
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    .hero{
+        height:400px;
+        margin: 0 auto;
+        background-color:#C0C0C0;
+        display:flex;
+        justify-content: center;
+        align-items:center;
+        padding:20px;
+    }
+    img{
+        width:200px;
+    }
+    .vue-info{
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        padding:50px;
+    }
+    a:link, a:visited{
+        width:150px;
+        margin-top:20px;
+        text-decoration: none;
+        color:#333;
+        background-color:#00cc00;
+        padding:30px;
+    }
+</style>
